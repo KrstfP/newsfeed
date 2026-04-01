9a0b8f9dbc2e8d82cbd92804616a1e3f1de85669
krstf <krstfp@proton.me>
2026-04-01 20:57:18 +0200

    A bit of reacto clean

--- srv/src/main/java/com/krstf/newsfeed/SecurityConfig.java
+++ srv/src/main/java/com/krstf/newsfeed/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.krstf.newsfeed;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -15,12 +17,14 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http
                 .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
+                        .requestMatchers("/app/**").permitAll()
+                        .anyRequest().authenticated()
+                ).addFilterBefore(new FirebaseAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
